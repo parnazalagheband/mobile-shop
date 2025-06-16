1ee6234f477e4dafcf7226873307e7daf0f82c25
Parnaz Alagheband <parnazalagheband@gmail.com>
2025-06-17 02:19:05 +0430

    feat: impleament empty-card

---
+++ components/view/empty-cart.vue
@@ -0,0 +1,7 @@
+<template>
+    <v-container class="d-flex justify-center align-center flex-column">
+    <v-img width="700" height="300" src="@/assets/images/empty-cart.jpg"></v-img>
+    <h2 class="text-h3 font-weight-bold text-grey-darken-2">سبد خرید شما خالی است!</h2>
+    <v-btn to="/" size="large" class="mt-4" color="light-blue-darken-3">بازگشت به فروشگاه</v-btn>
+    </v-container>
+</template>
